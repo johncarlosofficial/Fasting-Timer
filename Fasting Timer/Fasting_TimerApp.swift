c1c5c799376a4563363a7c2091d133d134a2fa73
João Carlos Magalhães <joaocarlosminformatica@gmail.com>
2022-08-02 00:20:11 -0300

    Initial Commit

---
+++ Fasting Timer/Fasting_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fasting_TimerApp.swift
+//  Fasting Timer
+//
+//  Created by João Carlos Magalhães on 02/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Fasting_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
